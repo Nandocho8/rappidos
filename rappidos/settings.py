1ac7e161e8e3fb4cdddd4e31d5d6836924f111c8
nicolas803 <ext-nicolas.davila@jetsmart.com>
2022-11-26 06:41:19 -0300

    deploy render coño este si porfavor

--- rappidos/settings.py
+++ rappidos/settings.py
@@ -47,7 +47,7 @@ SECRET_KEY = 'django-insecure-+j36hooei8jnkk_a#qq6qy5+b23)t4%drxk4gzdg*i_d5j9%9v
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'http://rappidos-env.eba-cawywhag.us-east-1.elasticbeanstalk.com/', 'localhost']
+    'http://rappidos-env.eba-cawywhag.us-east-1.elasticbeanstalk.com/', 'localhost','https://rappidosbackend.onrender.com/']
 
 
 # Application definition
